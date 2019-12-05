49b9f08466736e3a9cc1fb749744ba7191821089
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 20:50:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -82,7 +82,9 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
 def checkout(cart, coupons)
-
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  clearanced_cart = apply_clearance(couponed_cart)
 end
  # Consult README for inputs and outputs
   #
