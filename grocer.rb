94f7e6359c07c87b9e350b2836251710f6422900
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 20:45:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,7 +71,12 @@ end
 def apply_clearance(cart)
  i = 0 
   while i < cart.length
-  
+    if cart[i][:clearance]
+    cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.2)).round(2)
+  else 
+    cart
+    end 
+    i += 1
   end 
  end 
  # Consult README for inputs and outputs
